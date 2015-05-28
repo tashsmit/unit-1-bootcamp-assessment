29b9a8bee6e0e98edb2fed7aeccc1781f6e587bc
Amy Quispe <quispe.amy@gmail.com>
2015-05-28 16:21:00 -0400

    fixing the whole node idea

--- MyNode.java
+++ MyNode.java
@@ -11,4 +11,18 @@ public abstract class MyNode<T> {
     public abstract MyNode getRight();
 
     public abstract T getData();
+
+    public abstract void insert(MyNode<T> newNode);
+    public boolean contains(T someData){
+        if(getData()==null && someData == null){
+            return false;
+        }
+        if(getData()!= null && someData!=null && getData().equals(someData)) {
+            return true;
+        }
+        boolean left = getLeft()!=null ? getLeft().contains(someData) : false;
+        boolean right = getRight()!=null ? getRight().contains(someData) : false;
+
+        return left || right;
+    }
 }
